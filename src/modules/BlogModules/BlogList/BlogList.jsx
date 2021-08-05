89e53cf01b5b9189a251a33b4bf7886b9f42a76d
MadhushaPrasad <madushaprasad21@gmail.com>
2021-08-05 14:15:18 +0530

    medium api called for blog page

--- src/modules/BlogModules/BlogList/BlogList.jsx
+++ src/modules/BlogModules/BlogList/BlogList.jsx
@@ -1,31 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BlogCard from '../../../component/BlogCard/BlogCard';
-import blogs from './BlogListData.json';
 import './BlogListStyle.css';
+import axios from 'axios';
 
-const BlogList = () => (
-  <div className='blogListPolygonDiv'>
-    <div className='past-event-container'>
-      <h1 className='event-header'>Blogs List</h1>
-      <div className='container-fluid '>
-        <div className='row'>
-          {blogs.data.map((blog) => (
-            <div className='col-lg-4 col-md-3 col-sm-12' key={blog.id}>
-              <BlogCard
-                image={blog.image}
-                title={blog.title}
-                datetime={blog.datetime}
-                description={blog.description}
-                link={blog.link}
-                tags={blog.tags}
-                mediumProfileImg={blog.mediumProfileImg}
-              />
-            </div>
-          ))}
+
+const BlogList = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@dckalubowila25132')
+      .then(res => {
+        setPosts(res.data.items);
+      }).catch(err => {
+      console.log(err);
+    });
+  }, []);
+
+  return (
+    <div className='blogListPolygonDiv'>
+      <div className='past-event-container'>
+        <h1 className='event-header'>Blogs List</h1>
+        <div className='container-fluid '>
+          <div className='row'>
+            {posts.map((blog) => (
+              <div className='col-lg-4 col-md-3 col-sm-12' key={blog}>
+                <BlogCard
+                  image={blog.thumbnail}
+                  title={blog.title}
+                  datetime={blog.pubDate}
+                  link={blog.link}
+                  tags={blog.categories}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+    </div>);
+};
 
 export default BlogList;
